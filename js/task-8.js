fbd5d39952c925afda4d9f606ec32b89ef614086
Marchenko Iryna <irynamarchenko23@gmail.com>
2020-11-23 18:32:07 +0200

    dz-08
    Создала галерею с 2 доп заданиями

---
+++ js/task-8.js
@@ -0,0 +1,67 @@
+import galleryList from './gallery-items.js';
+//console.log(galleryList)
+
+const galleryContainer = document.querySelector('.js-gallery')
+const modal = document.querySelector('.js-lightbox')
+const modalImg = document.querySelector('.lightbox__image')
+const closeBtnModal = document.querySelector('button[data-action="close-lightbox"]')
+const closeModalOverlay = document.querySelector('.lightbox__overlay')
+
+const galleryItem = createGalleryItem(galleryList);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItem);
+
+galleryContainer.addEventListener('click', onContainerGalleryClick)
+closeBtnModal.addEventListener('click', offContainerGalleryClick)
+closeModalOverlay.addEventListener('click', offModalOverlayClick)
+
+
+//функция создания и рендер разметки по массиву данных и предоставленному шаблону
+function createGalleryItem(galleryList) {
+  return galleryList
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery_link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+  </a>
+  </li>`;
+    })
+    .join("");
+}
+
+//функция открытия модального окна по клику на элементе галереи
+function onContainerGalleryClick(evt) {
+  evt.preventDefault();
+
+  window.addEventListener('keydown', onPressEsc)
+
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  modal.classList.add('is-open');
+  modalImg.src = evt.target.dataset.source;
+}
+
+//функция закрытия модалки с изображением нажатием на кнопку button[data-action="close-lightbox"]
+function offContainerGalleryClick(evt) {
+  window.removeEventListener('keydown', onPressEsc)
+
+  modal.classList.remove('is-open');
+  modalImg.src = '';
+}
+
+//функция закрытия модального окна по клику на div.lightbox__overlay
+function offModalOverlayClick(evt) {
+  window.removeEventListener('keydown', onPressEsc)
+
+   modal.classList.remove('is-open');
+  modalImg.src = '';
+}
+
+// Закрытие модального окна по нажатию клавиши ESC
+function onPressEsc(evt) {  
+    if (evt.code === 'Escape') {
+      offContainerGalleryClick();
+    }  
+}
